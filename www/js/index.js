5d413d185f563c514443a47ca4424745468224fb
cardosovictorr <62979369+cardosovictorr@users.noreply.github.com>
2021-03-26 10:51:31 +1100

    starting to work in index.js

--- www/js/index.js
+++ www/js/index.js
@@ -4,5 +4,13 @@ function onDeviceReady() {
     // console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     // document.getElementById('deviceready').classList.add('ready');
 
+    $("#login_btn").click(function () {
 
+    });
+
+    $("#signup_btn").click(function () {
+        window.location.href = "index.html#signup"
+    });
+
+    // $("#")
 }
